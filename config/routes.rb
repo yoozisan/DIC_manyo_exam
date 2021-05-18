19b125b0d3d9dec6ac18c48c4f27664cc4420874
yoichiro kawai <acchonburike.416@gmail.com>
2021-05-19 00:24:10 +0900

    コントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'tasks/index'
+  get 'tasks/show'
+  get 'tasks/edit'
+  get 'tasks/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
