1449b4ec4ab6d6b21ef78bbfe914a65f50fc95ac
yoichiro kawai <acchonburike.416@gmail.com>
2021-05-19 12:10:01 +0900

    登録、一覧、詳細画面作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,31 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
   end
 
   def show
+    @task = Task.find(params[:id])
   end
 
   def edit
   end
 
   def new
+    @task = Task.new
   end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: "ブログを作成しました！"
+    else
+      render :new
+    end
+  end
+
+  private
+  def task_params
+      params.require(:task).permit(:title, :detail)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
-  get 'tasks/show'
-  get 'tasks/edit'
-  get 'tasks/new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :tasks
 end
