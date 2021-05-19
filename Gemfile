25d0659cddabf614e38ec926ada0cf2ee7b22125
yoichiro kawai <acchonburike.416@gmail.com>
2021-05-19 23:27:01 +0900

    テスト関連準備中

--- Gemfile
+++ Gemfile
@@ -26,12 +26,16 @@ group :development do
   gem 'listen', '>= 3.0.5', '< 3.2'
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'rspec-rails', '~> 3.8'
+  gem 'factory_bot_rails'
 end
 
 group :test do
+  # gem 'capybara', '>= 2.15'
+  # gem 'selenium-webdriver'
+  # gem 'chromedriver-helper'
   gem 'capybara', '>= 2.15'
-  gem 'selenium-webdriver'
-  gem 'chromedriver-helper'
+  gem 'webdrivers'
 end
 
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
---
+++ spec/system/task_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+describe 'タスク管理機能', type: :system do
+  describe '一覧表示機能' do
+    context '一覧画面に遷移した場合' do
+      it '作成済みのタスク一覧が表示される' do
+        # テストで使用するためのタスクを作成
+        task = FactoryBot.create(:task, title: 'task')
+        # タスク一覧ページに遷移
+        visit tasks_path
+        # visitした（遷移した）page（タスク一覧ページ）に「task」という文字列が
+        # have_contentされているか（含まれているか）ということをexpectする（確認・期待する）
+        expect(page).to have_content 'task'
+        # expectの結果が true ならテスト成功、false なら失敗として結果が出力される
+      end
+    end
+  end
+end
