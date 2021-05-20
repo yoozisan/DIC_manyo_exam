da526088da2b7280ffcf101f59a1a53dfa3e3ecc
yoichiro kawai <acchonburike.416@gmail.com>
2021-05-20 17:41:02 +0900

    アプリケーションの国際化・タイムゾーン設定

--- config/application.rb
+++ config/application.rb
@@ -7,8 +7,8 @@ Bundler.require(*Rails.groups)
 module DICManyoExam
   class Application < Rails::Application
     config.load_defaults 5.2
-    # config.time_zone = 'Tokyo'
-    # config.active_record.default_timezone = :local
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     # config.i18n.default_locale = :ja
     config.generators do |g|
       g.test_framework :rspec,
