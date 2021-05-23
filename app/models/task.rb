7ca8a381f8a62caa30214011c0854b701fb12936
yoichiro kawai <acchonburike.416@gmail.com>
2021-05-23 21:37:05 +0900

    viewに終了期限カラム表示を実装

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,5 @@
 class Task < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
+  validates :expired_at, presence: true
 end
--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ module DICManyoExam
     config.load_defaults 5.2
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
-    # config.i18n.default_locale = :ja
+    config.i18n.default_locale = :ja
     config.generators do |g|
       g.test_framework :rspec,
                        model_specs: true,
