b74fbde8eecd86dc2947663d8fb6e7ba3239f622
yoichiro kawai <acchonburike.416@gmail.com>
2021-05-25 12:07:03 +0900

    tasksテーブルカラムに検索インデックスを貼る

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_24_054202) do
+ActiveRecord::Schema.define(version: 2021_05_25_030333) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2021_05_24_054202) do
     t.datetime "updated_at", null: false
     t.datetime "expired_at", default: "2021-06-23 16:52:41", null: false
     t.string "status_name", default: "f", null: false
+    t.index ["title"], name: "index_tasks_on_title"
   end
 
 end
