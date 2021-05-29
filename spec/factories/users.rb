c1471eb7380afedf58257703ab06e5e56a0f48a9
yoichiro kawai <acchonburike.416@gmail.com>
2021-05-29 20:59:35 +0900

    ユーザー機能のテスト/system実装

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,12 +3,14 @@ FactoryBot.define do
     name { "user1" }
     email { "user1@user1.com" }
     password { "111111" }
+    password_confirmation { "111111" }
     admin { "一般" }
   end
   factory :admin_user, class: "User" do
     name { "user2"}
     email { "user2@user2.com"}
     password { "222222"}
+    password_confirmation { "222222" }
     admin { "管理者"}
   end
 end
