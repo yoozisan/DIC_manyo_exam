f41595cbbad87d1aba5b3f581da8ea9884491aff
yoichiro kawai <acchonburike.416@gmail.com>
2021-06-01 14:49:42 +0900

    ラベル機能のテスト/system実装

--- spec/factories/labels.rb
+++ spec/factories/labels.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :label do
-    name { "MyString" }
+    name { "ルンピニー" }
+  end
+  factory :label2, class: "Label" do
+    name { "トップキング" }
+  end
+  factory :label3, class: "Label" do
+    name { "ヨッカオ" }
   end
 end
